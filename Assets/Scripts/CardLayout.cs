8c698cac879503d917bf5ec0b529255f4b5d4287
Daniil Makagonov <daniil1makagonov@gmail.com>
2024-03-24 17:52:29 +0300

    update CardLayout.cs, CardView.cs

--- Assets/Scripts/CardLayout.cs
+++ Assets/Scripts/CardLayout.cs
@@ -7,6 +7,7 @@ public class CardLayout : MonoBehaviour
 {
     [SerializeField] public int _layoutId;
     [SerializeField] public Vector2 _offset;
+    [SerializeField] public bool _faceUp;
 
     private void Update()
     {
@@ -17,6 +18,7 @@ public class CardLayout : MonoBehaviour
             cards[i].transform.SetParent(transform);
             cards[i].transform.position = _offset * i;
             cards[i].transform.SetSiblingIndex(i);
+            cards[i].Rotate(_faceUp);
         }
     }
 }
--- Assets/Scripts/CardView.cs
+++ Assets/Scripts/CardView.cs
@@ -3,11 +3,17 @@
 public class CardView : MonoBehaviour
 {
   private CardInstance _cardInstance;
+  [SerializeField] private GameObject _sprite;
 
   public CardInstance CardInstance => _cardInstance;
 
   public void Init(CardInstance cardInstance)
   {
     _cardInstance = cardInstance;
+  }
+
+  public void Rotate(bool up)
+  {
+    _sprite.SetActive(up);
   }
 }
